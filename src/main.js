5f1ec1a8378e05c30bdd4bd85d72deb995b93ab8
Student <example@example.com>
2019-11-20 14:57:42 -0800

    add functional showFace() feature

--- src/main.js
+++ src/main.js
@@ -1,23 +1,28 @@
 import { Robogotchi } from './../src/robo.js';
 import $ from 'jquery';
 import './styles.css';
+import errorGIF from './error.gif';
+import smilingFace from './smilingFace.gif';
+
+const img = new Image();
+const img2 = new Image();
+img.src = errorGIF;
+img2.src = smilingFace;
 
 const updateStats = (robo) => {
+    $(`span#energy`).text(robo.energy);
     setInterval( () => {
         $(`span#energy`).text(robo.energy);
     }, 1000);
 }
 
-function test() {
+const showFace = async(robo, emotion) => {
+    const gifURL = await robo.getEmote(emotion);
+    $('#face').attr('src', gifURL);
     setTimeout( () => {
-        return 123;
-    }, 2000);
-}
-
-function stringTest(name) {
-    setTimeout( () => {
-        const greeting = `hello ${name}`;
-    }, 2000);
+        $('#face').attr('src', smilingFace);
+    }, 3000);
+    return true;
 }
 
 $(document).ready(function() {
@@ -25,15 +30,13 @@ $(document).ready(function() {
     $('input#name').on('change', function() {
         robo = new Robogotchi($('input#name').val());
 
-        updateStats(robo);
         $('div#robo').prepend(`<h1>${robo.name}</h1>`);
         $('input#name').remove();
+        updateStats(robo);
 
-        (async () => {
-            const gifURL = await robo.emote('happy');
-            console.log(gifURL);
-        })();
+        $('div#border').attr(`<img id='screen' alt='${robo.name} face'>`);
+        showFace(robo, 'hello');
+
+
     });
-
-
 });
